c083e91201c1cb1c2b7f53f046ff561026dc0cce
David Grudl <david@grudl.com>
2021-09-20 18:39:13 +0200

    removed deprecated IAuthenticator (BC break)

--- src/Security/User.php
+++ src/Security/User.php
@@ -20,7 +20,7 @@ use function func_get_args;
  * @property-read string|int|null $id
  * @property-read string[] $roles
  * @property-read ?int $logoutReason
- * @property   IAuthenticator $authenticator
+ * @property   Authenticator $authenticator
  * @property   Authorizator $authorizator
  */
 class User
@@ -63,7 +63,7 @@ class User
 
 	public function __construct(
 		private UserStorage $storage,
-		private ?IAuthenticator $authenticator = null,
+		private ?Authenticator $authenticator = null,
 		private ?Authorizator $authorizator = null,
 	) {
 	}
@@ -94,9 +94,7 @@ class User
 			$this->identity = $username;
 		} else {
 			$authenticator = $this->getAuthenticator();
-			$this->identity = $authenticator instanceof Authenticator
-				? $authenticator->authenticate(...func_get_args())
-				: $authenticator->authenticate(func_get_args());
+			$this->identity = $authenticator->authenticate(...func_get_args());
 		}
 
 		$id = $this->authenticator instanceof IdentityHandler
@@ -187,7 +185,7 @@ class User
 	/**
 	 * Sets authentication handler.
 	 */
-	public function setAuthenticator(IAuthenticator $handler): static
+	public function setAuthenticator(Authenticator $handler): static
 	{
 		$this->authenticator = $handler;
 		return $this;
@@ -197,7 +195,7 @@ class User
 	/**
 	 * Returns authentication handler.
 	 */
-	final public function getAuthenticator(): IAuthenticator
+	final public function getAuthenticator(): Authenticator
 	{
 		if (!$this->authenticator) {
 			throw new Nette\InvalidStateException('Authenticator has not been set.');
@@ -210,7 +208,7 @@ class User
 	/**
 	 * Returns authentication handler.
 	 */
-	final public function getAuthenticatorIfExists(): ?IAuthenticator
+	final public function getAuthenticatorIfExists(): ?Authenticator
 	{
 		return $this->authenticator;
 	}
