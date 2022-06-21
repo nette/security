fca6aa8cd5027b179ba74f1ee2ffdabcf8598a2b
David Grudl <david@grudl.com>
2022-06-21 16:15:47 +0200

    used promoted properties

--- src/Security/User.php
+++ src/Security/User.php
@@ -50,23 +50,16 @@ class User
 	/** @var callable[]  function (User $sender): void; Occurs when the user is logged out */
 	public iterable $onLoggedOut = [];
 
-	/** Session storage for current user */
-	private UserStorage $storage;
-	private ?Authenticator $authenticator;
-	private ?Authorizator $authorizator;
 	private ?IIdentity $identity = null;
 	private ?bool $authenticated = null;
 	private ?int $logoutReason = null;
 
 
 	public function __construct(
-		UserStorage $storage,
-		?Authenticator $authenticator = null,
-		?Authorizator $authorizator = null,
+		private UserStorage $storage,
+		private ?Authenticator $authenticator = null,
+		private ?Authorizator $authorizator = null,
 	) {
-		$this->storage = $storage;
-		$this->authenticator = $authenticator;
-		$this->authorizator = $authorizator;
 	}
 
 
